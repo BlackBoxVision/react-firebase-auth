c9306b2c0bcc3d2aee02bdf8b4bf9e083113fc06
jonatansalas <jonatansalas@live.com.ar>
2019-03-23 16:33:30 -0300

    feat: make story support selecting language

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,17 +1,80 @@
-import { Button, Dialog, DialogTitle, Grow } from '@material-ui/core';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  FormControl,
+  Grid,
+  Grow,
+  MenuItem,
+  Select,
+} from '@material-ui/core';
 import { storiesOf } from '@storybook/react';
-import React, { Fragment } from 'react';
+import React from 'react';
 import { FirebaseAuth } from '../src';
 
 interface ExtendedWindow extends Window {
   firebase: any;
 }
 
+interface AuthDialogProps {
+  languages: Array<any>;
+}
+
 declare var window: ExtendedWindow;
 
-class AuthDialog extends React.Component {
+const getLanguages = () => [
+  { value: 'vi', item: 'Vietnamese' },
+  { value: 'uk', item: 'Ukrainian' },
+  { value: 'tr', item: 'Turkish' },
+  { value: 'th', item: 'Thai' },
+  { value: 'sv', item: 'Swedish' },
+  { value: 'es_419', item: 'Spanish (Latin America)' },
+  { value: 'es', item: 'Spanish' },
+  { value: 'sl', item: 'Slovenian' },
+  { value: 'sk', item: 'Slovak' },
+  { value: 'sr', item: 'Serbian' },
+  { value: 'ru', item: 'Russian' },
+  { value: 'ro', item: 'Romanian' },
+  { value: 'pt_pt', item: 'ortuguese (Portugal)' },
+  { value: 'pt_br', item: 'Portuguese (Brazil)' },
+  { value: 'pl', item: 'Polish' },
+  { value: 'no', item: 'Norwegian (Bokmal)' },
+  { value: 'lt', item: 'Lithuanian' },
+  { value: 'lv', item: 'Latvian' },
+  { value: 'ko', item: 'Korean' },
+  { value: 'ja', item: 'Japanese' },
+  { value: 'it', item: 'Italian' },
+  { value: 'id', item: 'Indonesian' },
+  { value: 'hu', item: 'Hungarian' },
+  { value: 'hi', item: 'Hindi' },
+  { value: 'iw', item: 'Hebrew' },
+  { value: 'el', item: 'Greek' },
+  { value: 'de', item: 'German' },
+  { value: 'fr', item: 'French' },
+  { value: 'fi', item: 'Finnish' },
+  { value: 'fil', item: 'Filipino' },
+  { value: 'fa', item: 'Farsi' },
+  { value: 'en_gb', item: 'English (UK)' },
+  { value: 'en', item: 'English' },
+  { value: 'nl', item: 'Dutch' },
+  { value: 'da', item: 'Danish' },
+  { value: 'cs', item: 'Czech' },
+  { value: 'hr', item: 'Croatian' },
+  { value: 'zh_tw', item: 'Chinese (Traditional)' },
+  { value: 'zh_cn', item: 'Chinese (Simplified) ' },
+  { value: 'ca', item: 'Catalan' },
+  { value: 'bg', item: 'Bulgarian' },
+  { value: 'ar', item: 'Arabic' },
+];
+
+class AuthDialog extends React.Component<AuthDialogProps> {
+  static defaultProps = {
+    languages: getLanguages(),
+  };
+
   state = {
     open: false,
+    lng: 'es_419',
     uiConfig: null,
     firebase: null,
   };
@@ -59,23 +122,41 @@ class AuthDialog extends React.Component {
 
   handleClose = () => this.setState({ open: false });
 
+  handleChange = event => {
+    this.setState({ lng: event.target.value });
+  };
+
   render() {
-    const { open, uiConfig, firebase } = this.state;
+    const { lng, open, uiConfig, firebase } = this.state;
+    const { languages } = this.props;
 
     return (
-      <Fragment>
-        <Button
-          variant="contained"
-          onClick={open ? this.handleClose : this.handleOpen}
-        >
-          {open ? 'Close Auth Dialog' : 'Open Auth Dialog'}
-        </Button>
+      <>
+        <Grid container>
+          <Grid md={2} item>
+            <Button
+              variant="contained"
+              onClick={open ? this.handleClose : this.handleOpen}
+            >
+              {open ? 'Close Auth Dialog' : 'Open Auth Dialog'}
+            </Button>
+          </Grid>
+          <Grid md={2} item>
+            <FormControl>
+              <Select value={this.state.lng} onChange={this.handleChange}>
+                {languages.map(lang => (
+                  <MenuItem value={lang.value}>{lang.item}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
         <Dialog open={open} onClose={this.handleClose}>
           <DialogTitle id="dialog-title">Ingresar con: </DialogTitle>
           <Grow>
-            {uiConfig && firebase && (
+            {uiConfig && firebase && lng && (
               <FirebaseAuth
-                lng="es_419"
+                lng={lng}
                 version="3.4.1"
                 uiConfig={uiConfig}
                 firebaseAuth={firebase.auth()}
@@ -83,7 +164,7 @@ class AuthDialog extends React.Component {
             )}
           </Grow>
         </Dialog>
-      </Fragment>
+      </>
     );
   }
 }
